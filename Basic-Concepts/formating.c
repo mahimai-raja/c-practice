cdadebe23fbec5bb313c6d14219c5f17fba70157
Mahimai Raja J <mahimairaja3@gmail.com>
2022-10-31 07:49:18 +0530

    Formating style in C language

---
+++ Basic-Concepts/formating.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float a;
+    printf("Enter a float number : ");
+    scanf("%f",&a);
+    printf("The fomated Output : \n");
+    printf("%4.1f",a);
+    return 0;
+}
